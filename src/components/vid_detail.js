34a46e8bc58131b083de974bfa7fb411ce409b57
Aviral <helloworld@aviral.me>
2017-02-03 06:56:03 +0530

    minor fixes

--- src/components/vid_detail.js
+++ src/components/vid_detail.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const VideoDetail = ({currVideo}) => {
   if(!currVideo){
-    return(<div> Loading... </div>);
+    return(
+      <div className="embed-responsive embed-responsive-16by9">
+        Loading...
+      </div>
+    );
   }else{
     console.log(currVideo);
     const url = "https://www.youtube.com/embed/"+currVideo.id.videoId;
